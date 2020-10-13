664215525f9ade03e27d746666710007b84b99b1
Jyothis P <jyothisp52@gmail.com>
2020-10-13 15:26:05 +0530

    pretty

---
+++ day.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+r, w = os.pipe()
+process_id = os.fork()
+if (process_id > 0):
+    n = str(input("enter the numbers"))
+    os.close(r)
+    os.write(w, n)
+else:
+    a = ""
+    n = os.read(r, 200)
+    num = []
+    sum = 0
+    avg = 0
+    num.append(n.split(','))
+    num = num[0]
+    for i in range(len(num)):
+        num[i] = int(num[i])
+    for i in range(len(num)):
+        sum = num[i] + sum
+    avg = sum / len(num)
+    print(sum)
+    print(avg)
